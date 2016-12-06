f5018eb3a60c385a50dda2447eea3c080497a974
unknown <morris821028@gmail.com>
2016-12-06 16:01:19 +0800

    add status ui field control

---
+++ hw2-unity/ICG-HW2/Assets/_Scripts/UI/SingleAmmoStatusUI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SingleAmmoStatusUI : MonoBehaviour
+{
+    public GameObject target = null;
+    public int mAmmoIndex = 0;
+    private Text mAmmoAmount;
+    private tankShoot mTankShoot = null;
+    // Use this for initialization
+    void Start()
+    {
+        mAmmoAmount = GetComponentInChildren<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mTankShoot == null)
+            mTankShoot = target.GetComponent<tankShoot>();
+        else
+            mAmmoAmount.text = "" + mTankShoot.mAmmoAmount[mAmmoIndex];
+    }
+}
